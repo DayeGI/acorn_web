462ab5cb37b517391f2a7f710efdf73370a3f4c1
Shawn L <shawnl@istuary.com>
2015-02-12 11:57:18 -0800

    Grunt file set up and ready to rock!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,21 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		//this is the task to run
 		sass: {
 			dist: {
 				files: {
-					'style/style.css' : 'plugins/acorn-default/public/css/style.scss'
+					//output	 -- you can name it what you want   //input -- put all the dependancies here so we only need to compile from one source
+					'plugins/acorn-default/public/css/style.css' : 'plugins/acorn-default/public/css/sass/main.scss'
+					// you can distribute css files if you like
 				}
 			}
 		},
 		watch: {
 			css: {
+				//watch for these files to change
 				files: '**/*.scss',
+				//when a file changes, run this task below, called 'sass'
 				tasks: ['sass']
 			}
 		}
