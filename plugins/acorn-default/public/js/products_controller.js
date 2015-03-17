293d4781f4ce04300727eba16215bc36f20a0d6f
Shawn L <shawnl@istuary.com>
2015-03-17 14:04:55 -0700

    fixed some css for products landing

--- plugins/acorn-default/public/js/products_controller.js
+++ plugins/acorn-default/public/js/products_controller.js
@@ -4,6 +4,6 @@
 function ProductsController ($scope, $http) {
 	$scope.productLines = [];
 	$http({method:'GET', url:'/api/products/all'})
-			.success(function(results){console.log(results);$scope.productLines = results.message})
+			.success(function(results){$scope.productLines = results.message})
 			.error(function(err){console.log(err)});
 }
