67948d835be05ee877566db495f21e10d05eaefb
yeeloong <yeeloong.001@gmail.com>
2015-03-19 01:47:00 -0700

    added logic controls for career page

--- plugins/acorn-default/public/js/careers.js
+++ plugins/acorn-default/public/js/careers.js
@@ -3,28 +3,30 @@ $( document ).ready(function() {
 	//Bootstrap Dropdown-checkbox
 	//http://acquisio.github.io/bootstrap-dropdown-checkbox/
 
-	var myData = [{id: 1, label: "产品" },
-				  {id: 2, label: "研发" },
-				  {id: 3, label: "市场" },
-				  {id: 4, label: "客服" },
-				  {id: 5, label: "销售" },
-				  {id: 6, label: "运营" }];
+	var myData = [{id: 1, label: "产品", isChecked: true},
+				  {id: 2, label: "研发", isChecked: true},
+				  {id: 3, label: "市场", isChecked: true},
+				  {id: 4, label: "客服", isChecked: true},
+				  {id: 5, label: "销售", isChecked: true},
+				  {id: 6, label: "运营", isChecked: true}];
 	$(".jobTypeDropdownCheckbox").dropdownCheckbox({
 	  data: myData,
 	  title: "职位类别",
 	  btnClass: "dropdown-btn-style"
 	});
 
-	var myData2 = [{id: 1, label: "北京" },
-				   {id: 2, label: "上海" },
-				   {id: 3, label: "深圳" },
-				   {id: 4, label: "杭州" },
-				   {id: 5, label: "宁波" },
-				   {id: 6, label: "广州" }];
+	var myData2 = [{id: 1, label: "北京", isChecked: true},
+				   {id: 2, label: "上海", isChecked: true},
+				   {id: 3, label: "深圳", isChecked: true},
+				   {id: 4, label: "杭州", isChecked: true},
+				   {id: 5, label: "宁波", isChecked: true},
+				   {id: 6, label: "广州", isChecked: true}];
 	$(".jobAreaDropdownCheckbox").dropdownCheckbox({
 	  data: myData2,
 	  title: "工作地点",
 	  btnClass: "dropdown-btn-style dropdown-btn-style2"
 	});
 
+	var returnChecks = $(".jobTypeDropdownCheckbox").dropdownCheckbox("checked");
+	console.log(returnChecks);
 });
